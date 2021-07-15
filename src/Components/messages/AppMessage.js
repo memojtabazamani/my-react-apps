341c4e83e516e8a96ccec7a96d1330ca552fd698
memojtabazamani <memojtabazamani@gmail.com>
2021-07-15 10:45:08 +0430

    done: make a sample components

--- src/Components/messages/AppMessage.js
+++ src/Components/messages/AppMessage.js
@@ -10,6 +10,15 @@ class AppMessage extends React.Component {
                 <Message title="New Message!" type="warning" closeIcon={true} icon={"address book"}>
                     You Have Message from "Jhon Doe"
                 </Message>
+                <Message title="New Download!" type="info" closeIcon={true} icon={"download"}>
+                    [New link... Download This]
+                </Message>
+                <Message title="Notification" type="success" closeIcon={true} icon={"bell"}>
+                    You Have 1060 Unread Message
+                </Message>
+                <Message title="Checked Task" type="positive" closeIcon={true} icon={"calendar check"}>
+                    Task [Go Gym] Completed
+                </Message>
             </>
         )
     }
--- src/Components/messages/Message.js
+++ src/Components/messages/Message.js
@@ -13,9 +13,9 @@ class Message extends React.Component {
         return (
             <div
                 className={`ui ${this.props.type} message ${this.state.hide ? `hidden` : ``} ${this.props.icon ? "icon message" : ""}`}>
-                { this.props.icon ?  <i className={`${this.props.icon} icon`}></i> : "" }
+                { this.props.icon ?  <i className={`${this.props.icon} icon`}/> : "" }
                 {this.props.closeIcon &&
-                <i className="close icon" onClick={e => this.setState({hide: true})}>&nbsp;</i>}
+                <i className="close icon" onClick={e => this.setState({hide: true})}/>}
                 <div className="content">
                     <div className="header">
                         {this.props.title}
@@ -30,7 +30,7 @@ class Message extends React.Component {
 }
 
 /*
-* Types of message
+* Types of message :
 * success
 * info
 * warning
