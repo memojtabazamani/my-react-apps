100ed9ecc75d6ab5411013f2621c4b86845ad8ca
memojtabazamani <memojtabazamani@gmail.com>
2021-07-17 15:25:12 +0430

    done: loading when show post

--- src/Components/postapplication/CardPost.js
+++ src/Components/postapplication/CardPost.js
@@ -4,26 +4,44 @@ import axios from 'axios';
 const CardPost = ({postId}) => {
 
     const [post, setPost] = useState([]);
+    const [loader, setLoader] = useState(true);
 
+    // use async and wait keywords
     const retreiveData = async (postId) => {
         let {data} = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
         setPost(data);
-    }
+        // Here hidden loading
+        setLoader(false);
+    };
 
+    // When This component update or mounted!
     useEffect(() => {
+        // call retrive function for show this data
         retreiveData(postId);
+        // Here show loading
+        setLoader(true);
     }, [postId]);
 
+
+    if (loader) {
+        return (<div className="ui segment full one column">
+                <p></p>
+                <div className="ui active dimmer">
+                    <div className="ui loader"></div>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="card">
             <div className="content">
-                <h1 className="ui header">{ post.title } - { post.id } </h1>
+                <h1 className="ui header">{post.title} - {post.id} </h1>
                 <div className="description">
                     {post.body}
                 </div>
             </div>
-        </div>
-    )
+        </div>)
+
 }
 
 export default CardPost;
