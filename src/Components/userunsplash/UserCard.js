663e6ed1e9e2431067897e291c861a1e73cc77c5
memojtabazamani <memojtabazamani@gmail.com>
2021-07-23 20:20:29 +0430

    done: اضافه کردن سوشال مدیا برای نمایش

--- src/Components/userunsplash/UserCard.js
+++ src/Components/userunsplash/UserCard.js
@@ -1,38 +1,69 @@
-import React,{ Component } from 'react';
+import React, {Component} from 'react';
 
 class UserCard extends Component {
     user = [];
+
     constructor(props) {
         super(props);
         this.user = props.user;
     }
+
     render() {
         let date = new Date(this.user.update_a);
-        console.log(date);
+        const icons = {
+            "instagram_username": "instagram ",
+            "portfolio_url": "address book",
+            "twitter_username": "twitter",
+            "paypal_email": "paypal"
+        }
+        let soc = this.user.social;
+        let exeSoc = [];
+        for (var x in soc) {
+            if (soc[x]) {
+                var elm =
+                    <div className="item">
+                        <i className={`${icons[x]} icon`}></i>
+                        <div className="content">{soc[x]}</div>
+                    </div>;
+                exeSoc.push(elm);
+            }
+        }
         return (
             <>
-                <div className="ui link cards five column container">
+                <div className="ui link cards sex column container">
                     <div className="card column">
                         <div className="image">
-                            <img src={this.user.profile_image.large} />
+                            <img src={this.user.profile_image.large}/>
                         </div>
                         <div className="content">
                             <div className="header">{this.user.username}</div>
                             <div className="description">
-                                {this.user.bio}
+                                {this.user.bio ?? "Hello There"}
                             </div>
                         </div>
-                        <div className="extra content">
-      <span className="right floated">
-        Joined in {date.getFullYear()}
-      </span>
-                            <span>
-        <i className="user icon"></i>
-                                {this.user.followers_count} Followers
-      </span>
+                        <div className="content">
+                            <div className="ui list">
+
+                                <div className="item">
+                                    <i className="user icon"></i>
+                                    <div className="content">
+                                        {this.user.following_count}
+                                    </div>
+                                </div>
+
+                                <div className="item">
+                                    <i className="user icon"></i>
+                                    <div className="content">
+                                        {this.user.followers_count}
+                                    </div>
+                                </div>
+
+                                {exeSoc}
+                            </div>
+
                         </div>
                     </div>
-                    <table className="ui blue table card twelve wide column" style={{
+                    <table className="ui blue table card ten wide column" style={{
                         flex: "auto"
                     }}>
                         <tbody>
